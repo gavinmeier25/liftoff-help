d9bc85c52f9d7bb86f97845ae5d32b28b7bbd721
gavinmeier25 <gavinmeier41910@gmail.com>
2019-08-01 22:35:38 -0500

    last change

--- src/Header.tsx
+++ src/Header.tsx
@@ -16,18 +16,9 @@ export class Header extends Component<Props> {
             <Link to="/docs/get-started/introduction/">{siteTitle}</Link>
           </Menu.Item>
           <Menu.Item>
-            <a
-              href="https://github.com/jannikbuschke/gatsby-antd-docs"
-              target="_blank"
-            >
+            <a href="https://github.com/gavinmeier25" target="_blank">
               <Icon type="github" />
               GitHub
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a href="https://twitter.com/jannikbuschke" target="_blank">
-              <Icon type="twitter" />
-              Twitter
             </a>
           </Menu.Item>
         </Menu>
